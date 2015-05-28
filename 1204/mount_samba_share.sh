0969e8050165824b0a0410cda1c4c31edb15d22f
Zundra Daniel <zundra@bluebox.com>
2015-05-28 18:45:46 -0400

    mount samba share

---
+++ 1204/mount_samba_share.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mount -t smbfs '//172.28.128.2/code' /Users/zundra/code
--- 1204/samba.sh
+++ 1204/samba.sh
@@ -1,7 +1,17 @@
+
 #!/bin/bash
+echo "===================================================================="
+echo "======================= Installing Samba ==========================="
+echo "===================================================================="
+
 sudo apt-get install samba
 sudo cp config/samba.cfg /etc/samba/smb.conf
 mkdir -p /home/vagrant/code
 chmod 0777 -R /home/vagrant/code 
 sudo service sbmd restart
  
+echo "===================================================================="
+echo "====================== Complete install Samba ======================"
+echo "===================================================================="
+echo 
+echo
